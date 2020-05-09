465b4e47388390bd51d9bc2b38d19c7747197226
antonror <hitty@i.ua>
2020-05-09 09:16:15 +0500

    Completed the flow for sign up, log in and edit Merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :status, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :password, :current_password, :description, :status])
+    end
 end
